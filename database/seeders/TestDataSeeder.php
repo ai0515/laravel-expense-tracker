79fd26360e348800b6f0a4e96459110b8ef42111
ai0515 <ayumi.iwami+gt@gmail.com>
2025-08-23 20:16:18 +0900

    SampleDataSeeder.phpの内容をTestDataSeeder.phpに移行

--- database/seeders/TestDataSeeder.php
+++ database/seeders/TestDataSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\PaymentMethod;
+use App\Models\Category;
+use App\Models\Transaction;
+use App\Enums\TransactionType;
 
 class TestDataSeeder extends Seeder
 {
@@ -18,9 +22,32 @@ class TestDataSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call([
-            // サンプル収支データの登録
-            SampleDataSeeder::class,
-        ]);
+        $user = User::first();
+
+        //出金データの登録
+        $paymentMethod = PaymentMethod::first();
+        $categories = Category::limit(2)->get();
+
+        $transaction = Transaction::factory()
+            ->for($user, 'user')
+            ->for($paymentMethod, 'paymentMethod')
+            ->state(['amount' => 1000])
+            ->state(['note' => 'コンビニ'])
+            ->state(['transaction_type' => TransactionType::PAYMENT])
+            ->create();
+        $transaction->categories()->attach($categories->pluck('id'));
+
+        //入金データの登録
+        $paymentMethod = PaymentMethod::where('payment_method', '銀行口座')->first();
+        $category = Category::where('category', '給料')->first();
+        
+        $transaction = Transaction::factory()
+            ->for($user, 'user')
+            ->for($paymentMethod, 'paymentMethod')
+            ->state(['amount' => 20000])
+            ->state(['note' => 'アルバイト'])
+            ->state(['transaction_type' => TransactionType::INCOME])
+            ->create();
+        $transaction->categories()->attach($category->id);
     }
 }
